8a9ac11df3161c6484bde1c3f30e232599509584
JordanGoh4 <lowonmath@gmail.com>
2026-03-26 21:24:30 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -20,8 +20,8 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.StudentClass;
-import seedu.address.model.tag.Tag;
 import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.tag.Tag;
 
 /**
  * Imports persons from a CSV file.
